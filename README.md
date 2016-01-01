a09752bef86044f7bdc8d7c3196b8635147dee3a
tamimeur <tamimeur@gmail.com>
2015-12-31 17:17:04 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Beluga
+
+Automated, experimentally-driven design of genetic circuits.
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pipsi install .
+
+
+# Usage
+
+To use it:
+
+    $ beluga --help
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+Automated, experimentally-driven design of genetic circuits.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='beluga',
+    version='0.1.0',
+    url='https://github.com/tamimeur/beluga',
+    license='BSD',
+    author='Tileli Amimeur',
+    author_email='tamimeur@gmail.com',
+    description='Automated, experimentally-driven design of genetic circuits.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'beluga = cli_beluga.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
